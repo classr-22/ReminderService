4dafb54e7de0c39eefc40c1de24a6abb9ee954e9
classr-22 <roundup414@gmail.com>
2023-07-12 14:38:26 +0530

    Setup opf smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
+
 
 const setupAndStartServer = () => {
     const app = express();
@@ -10,6 +12,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log('server is running on port :',PORT);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'raamitut@gmail.com',
+            'This is a testing email',
+            'Hey, how are you ,I hope you like the support'
+        )
     });
 }
 
