1f4204c23e67363abe7cc1cd5d07de143d831344
classr-22 <roundup414@gmail.com>
2023-07-13 13:17:18 +0530

    major changes and start subscribing the messages from rabbit mq message broker

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const { PORT } = require('./config/serverConfig');
-//const { sendBasicEmail } = require('./services/email-service');
+const { PORT, REMINDER_BINDING_KEY } = require('./config/serverConfig');
+
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 const jobs = require('./utils/job'); 
 
+const {subscribeMessage,createChannel} = require('./utils/messageQueue');
 
-const setupAndStartServer = () => {
+
+const setupAndStartServer = async() => {
     const app = express();
     
     app.use(bodyparser.urlencoded({extended: true}));
     app.use(bodyparser.json());
-    
+
     app.post('/api/v1/tickets',TicketController.create);
+    const channel = await createChannel(); 
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
 
     app.listen(PORT, () => {
         console.log('server is running on port :',PORT);
-        jobs();
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'raamitut@gmail.com',
-        //     'This is a testing email',
-        //     'Hey, how are you ,I hope you like the support'
-        // )
+        //jobs();
     });
 }
 
